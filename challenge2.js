5799343ad5d84f601fa4b25a04ab05124cb95f1b
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2021-12-07 14:42:17 -0500

    Fix challenge 2 - Finally pass all the tests

--- challenge2.js
+++ challenge2.js
@@ -1,28 +1,42 @@
-const carta = 'bici coche balón _playstation bici coche peluche';
+const carta = '   balon _play3   _play4 balon bici   bici   '; 
 
 function listGifts(letter) {
-    // s+ (uno o más espacios en blanco) /g (reemplaza todas las ocurrencias)
-    letter = letter.replace(/\s+/g, " ");
+    letter = letter.replace(/\s+/g, ' ');   //Eliminar más de un espacio en blanco ENTRE LAS PALABRAS
+    letter = letter.trim(); //Eliminar uno o más espacios en blanco AL INICIO Y FINAL DEL STRING
+
     const words = letter.split(' ');
+    const final_letter = []; //Array en el que se almacenarán las palabras válidas (que no estén tachadas)
 
     words.map((word) => {
         //Por cada palabra se eliminan sus espacios en blanco y si comienza por _ se elimina
-        let toy = word.trim();
-        
-        if (toy[0] == '_') { 
-            words.splice(words.indexOf(toy), 1);
+        if (word[0] != '_') {
+            //Si no está tachada la incluye a la lista final
+            final_letter.push(word);
         }
     });
 
-    //Contar cuántas veces aparece cada palabra
-    const uniqueValues = Array.from(new Set(words));    //Se crea un array a partir de un set (sin elementos repetidos)
-    const count = {} //Objeto a retornar
+    //-- Para Contar cuántas veces aparece cada palabra
+    const uniqueValues = Array.from(new Set(final_letter)); //Se crea un array a partir de un set (sin elementos repetidos)
+    const count = {}; //Objeto a retornar
 
-    uniqueValues.map((value) => { //Por cada valor único
-        count[value] = words.filter( word => word == value).length; //La cantidad de repeticiones es la longitud de la lista que solo contiene esa palabra
-    }); 
+    uniqueValues.map((value) => {
+        //Por cada valor único
+        count[value] = final_letter.filter((word) => word == value).length; //La cantidad de repeticiones es la longitud de la lista que solo contiene esa palabra
+    });
 
-    return count; 
+    return count;
 }
 
-console.log(listGifts(carta)); 
+let obtained = listGifts(carta);
+
+const expected = {
+    bici: 2,
+    coche: 2,
+    balón: 1,
+    peluche: 1,
+};
+
+
+console.log(obtained);
+console.log(expected);  
+console.log(obtained ==  expected); 
