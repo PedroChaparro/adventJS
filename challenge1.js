99d9b3ad7d076a5b8d9cd3c741516f7a5398187e
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2021-12-06 19:09:24 -0500

    Update challenge 1 (regEx)
    Implementación del challenge 1 mediante expresiones regulares

--- challenge1.js
+++ challenge1.js
@@ -8,12 +8,18 @@ const ovejas = [
 ];
 
 function contarOvejas(ovejas) {
+    
+    //Expresión regular para verificar que contenga la a y la n (sin importar mayúsculas)
+    const regEx = new RegExp(/(?=.*a)(?=.*n)/, 'gi'); 
+
     const sheeps = ovejas.filter((sheep) => {
         return (
-            sheep.color == 'rojo' && sheep.name.toLowerCase().includes('a') && sheep.name.toLowerCase().includes('n')
+            sheep.color == 'rojo' && regEx.test(sheep.name)
+            /* sheep.color == 'rojo' && sheep.name.toLowerCase().includes('a') && sheep.name.toLowerCase().includes('n') */
         );
     });
 
     return sheeps; 
 }
 
+console.log(contarOvejas(ovejas)); 
