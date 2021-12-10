c4df1a17bc1f773697e04d32a990137d175db43e
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2021-12-09 20:20:13 -0500

    Challenge 2 short version

--- challenge2.js
+++ challenge2.js
@@ -1,42 +1,22 @@
-const carta = '   balon _play3   _play4 balon bici   bici   '; 
+const carta = 'bici coche balón _playstation bici coche peluche'; 
 
 function listGifts(letter) {
     letter = letter.replace(/\s+/g, ' ');   //Eliminar más de un espacio en blanco ENTRE LAS PALABRAS
     letter = letter.trim(); //Eliminar uno o más espacios en blanco AL INICIO Y FINAL DEL STRING
 
     const words = letter.split(' ');
-    const final_letter = []; //Array en el que se almacenarán las palabras válidas (que no estén tachadas)
+    const count = {}; 
 
     words.map((word) => {
         //Por cada palabra se eliminan sus espacios en blanco y si comienza por _ se elimina
         if (word[0] != '_') {
-            //Si no está tachada la incluye a la lista final
-            final_letter.push(word);
+            //Si no está tachada, la agrega al objeto a retornar 
+            count[word] == undefined ? count[word] = 1 : count[word]++;     //Si no existe la llave, la crea e inicia el contador como 1
         }
-    });
-
-    //-- Para Contar cuántas veces aparece cada palabra
-    const uniqueValues = Array.from(new Set(final_letter)); //Se crea un array a partir de un set (sin elementos repetidos)
-    const count = {}; //Objeto a retornar
-
-    uniqueValues.map((value) => {
-        //Por cada valor único
-        count[value] = final_letter.filter((word) => word == value).length; //La cantidad de repeticiones es la longitud de la lista que solo contiene esa palabra
     });
 
     return count;
 }
 
 let obtained = listGifts(carta);
-
-const expected = {
-    bici: 2,
-    coche: 2,
-    balón: 1,
-    peluche: 1,
-};
-
-
-console.log(obtained);
-console.log(expected);  
-console.log(obtained ==  expected); 
+console.log(obtained); 
